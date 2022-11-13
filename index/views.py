a68876fd161e9d60f42cc177ebc6c90e2a51e7a6
lordvaynard <ronieseixas@gmail.com>
2022-11-13 11:30:03 -0300

    tentativa subir site na vercel

--- index/views.py
+++ index/views.py
@@ -6,12 +6,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    now = datetime.now()
     html = f'''
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <p><a href="home/empresa">Link</a></p>
         </body>
     </html>
     '''
